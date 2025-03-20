caaafdb8b0f94681f52d148a4b46aa905dd3b0f4
czx <953048024@qq.com>
2025-03-20 18:16:16 +0800

    update

--- uly-cls-export.py
+++ uly-cls-export.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11l-obb.pt")
+model = YOLO("yolo11l-cls.pt")
 
 # Export the model
 model.export(format="onnx", opset = 9, simplify = True)
--- uly-ob-export.py
+++ uly-ob-export.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8m.pt")
+model = YOLO("yolo11l.pt")
 
 # Export the model
 model.export(format="onnx", opset = 9, simplify = True)
